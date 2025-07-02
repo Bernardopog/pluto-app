d791591b0ac677cc3017959f2532dbd72bda2b9a
R3ygoski <hckranonymous2@hotmail.com>
2025-07-02 19:34:31 -0300

    chore(frontend/ui): make ActionButton more dynamic

--- src/app/ui/ActionButton/index.tsx
+++ src/app/ui/ActionButton/index.tsx
@@ -5,6 +5,8 @@ interface IActionButtonProps {
   icon: ReactNode;
   label: string;
   disabled?: boolean;
+  className?: string;
+  children?: ReactNode;
 }
 
 export default function ActionButton({
@@ -12,18 +14,21 @@ export default function ActionButton({
   icon,
   label,
   disabled,
+  className,
+  children,
 }: IActionButtonProps) {
   return (
     <button
       type="button"
       onClick={action}
-      className="flex items-center p-1 pr-2 gap-x-2 rounded-full font-medium bg-chetwode-blue-200 text-chetwode-blue-950 ease-in-out duration-300 hover:brightness-95 active:brightness-75 disabled:grayscale-100 disabled:cursor-not-allowed disabled:bg-chetwode-blue-200"
+      className={`flex items-center p-1 pr-2 gap-x-2 rounded-full font-medium bg-chetwode-blue-200 text-chetwode-blue-950 ease-in-out duration-300 hover:brightness-95 active:brightness-75 disabled:grayscale-100 disabled:cursor-not-allowed disabled:bg-chetwode-blue-200 ${className}`}
       disabled={disabled}
     >
       <span className="p-0.5 rounded-full text-2xl bg-chetwode-blue-800 text-chetwode-blue-50">
         {icon}
       </span>
       {label}
+      {children}
     </button>
   );
 }
