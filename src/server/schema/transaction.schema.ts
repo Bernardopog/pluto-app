7a751a1ea1e99b36b674e93235ae9d2a3866825c
Bernardo Poggioni <bernardo_poggioni500@hotmail.com>
2025-08-08 10:34:02 -0300

    fix(backend/transaction): change date type in schema

--- src/server/schema/transaction.schema.ts
+++ src/server/schema/transaction.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const transactionSchema = z.object({
   name: z.string(),
   value: z.number(),
-  date: z.transform((dateStr) => new Date(`${dateStr}T00:00:00.000Z`)),
+  date: z.coerce.date(),
 
   categoryId: z.number(),
 });
