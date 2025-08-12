ee89cf4b7fc20b437d0e13b9eff14ce0552476cd
Bernardo Poggioni <bernardo_poggioni500@hotmail.com>
2025-08-12 10:34:03 -0300

    style(frontend/dashboard): add context about items limit in transaction section

--- src/app/components/Dashboard/DashboardTransaction/DashboardTransactionTitle.tsx
+++ src/app/components/Dashboard/DashboardTransaction/DashboardTransactionTitle.tsx
@@ -14,35 +14,42 @@ export default function DashboardTransactionTitle() {
   };
 
   return (
-    <div className="flex items-center gap-x-2">
-      <h2 className="subtitle">Transações</h2>
-      <button
-        type="button"
-        title="Formulário de adição rápida"
-        className={`flex justify-center items-center w-7 h-7 rounded-full duration-300 ease-in-out text-lg border ${
-          isTransactionFormOpen
-            ? "text-chetwode-blue-50 bg-chetwode-blue-700 hover:bg-chetwode-blue-800 active:bg-chetwode-blue-900 border-transparent"
-            : "text-chetwode-blue-950 bg-chetwode-blue-200 hover:bg-chetwode-blue-300 active:bg-chetwode-blue-400 border-chetwode-blue-900"
-        }`}
-        aria-label="Formulário de adição rápida"
-        aria-expanded={isTransactionFormOpen}
-        onClick={toggleTransactionForm}
-      >
-        <MdTextSnippet />
-      </button>
-      <button
-        type="button"
-        title="Filtro simples de transações"
-        className={`flex justify-center items-center w-7 h-7 rounded-full duration-300 ease-in-out text-lg border ${
-          transactionFilter !== "all"
-            ? "text-chetwode-blue-50 bg-chetwode-blue-700 hover:bg-chetwode-blue-800 active:bg-chetwode-blue-900 border-transparent"
-            : "text-chetwode-blue-950 bg-chetwode-blue-200 hover:bg-chetwode-blue-300 active:bg-chetwode-blue-400 border-chetwode-blue-900"
-        }`}
-        aria-label="Filtro simples de transações"
-        onClick={handleModal}
-      >
-        <MdFilterAlt />
-      </button>
+    <div className="flex flex-col justify-center">
+      <h2 className="subtitle">
+        Transações{" "}
+        <span className="italic text-sm text-chetwode-blue-900/75 grayscale-50">
+          (primeiras 12 transações)
+        </span>
+      </h2>
+      <div className="flex gap-x-2">
+        <button
+          type="button"
+          title="Formulário de adição rápida"
+          className={`flex justify-center items-center w-7 h-7 rounded-full duration-300 ease-in-out text-lg border ${
+            isTransactionFormOpen
+              ? "text-chetwode-blue-50 bg-chetwode-blue-700 hover:bg-chetwode-blue-800 active:bg-chetwode-blue-900 border-transparent"
+              : "text-chetwode-blue-950 bg-chetwode-blue-200 hover:bg-chetwode-blue-300 active:bg-chetwode-blue-400 border-chetwode-blue-900"
+          }`}
+          aria-label="Formulário de adição rápida"
+          aria-expanded={isTransactionFormOpen}
+          onClick={toggleTransactionForm}
+        >
+          <MdTextSnippet />
+        </button>
+        <button
+          type="button"
+          title="Filtro simples de transações"
+          className={`flex justify-center items-center w-7 h-7 rounded-full duration-300 ease-in-out text-lg border ${
+            transactionFilter !== "all"
+              ? "text-chetwode-blue-50 bg-chetwode-blue-700 hover:bg-chetwode-blue-800 active:bg-chetwode-blue-900 border-transparent"
+              : "text-chetwode-blue-950 bg-chetwode-blue-200 hover:bg-chetwode-blue-300 active:bg-chetwode-blue-400 border-chetwode-blue-900"
+          }`}
+          aria-label="Filtro simples de transações"
+          onClick={handleModal}
+        >
+          <MdFilterAlt />
+        </button>
+      </div>
     </div>
   );
 }
