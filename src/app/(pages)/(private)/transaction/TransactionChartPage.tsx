f42c49847d4bbc4134612e4f6b80ac35bad41656
Bernardo Poggioni <hckranonymous2@hotmail.com>
2025-08-14 16:57:35 -0300

    feat(frontend/transaction): change transaction chart and add more functionalities

--- src/app/(pages)/(private)/transaction/TransactionChartPage.tsx
+++ src/app/(pages)/(private)/transaction/TransactionChartPage.tsx
@@ -1,39 +1,31 @@
 "use client";
 
-import { Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
 import { useTransactionBudgetStore } from "@/app/stores/useTransactionBudgetStore";
-import { Dispatch, SetStateAction, useState } from "react";
-import Select from "@/app/ui/Select";
+import { useState } from "react";
 import Input from "@/app/ui/Input";
 import { MdCalendarToday } from "react-icons/md";
+import dynamic from "next/dynamic";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+import { ApexOptions } from "apexcharts";
+import Radio from "@/app/ui/Radio";
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+type ChartType =
+  | "all"
+  | "income"
+  | "expense"
+  | "budget-all"
+  | "budget-income"
+  | "budget-expense";
 
 export default function TransactionChartPage() {
   const [passedDays, setPassedDays] = useState<number>(7);
-  const [chartType, setChartType] = useState<"all" | "income" | "expenses">(
-    "all"
-  );
+  const [chartType, setChartType] = useState<ChartType>("all");
 
   const transactionList = useTransactionBudgetStore(
     (s) => s.transactionData.list
   );
+  const budgetList = useTransactionBudgetStore((s) => s.budgetData.list);
 
   const getLastDays = (lastDays: number) => {
     const days = [];
@@ -75,6 +67,34 @@ export default function TransactionChartPage() {
     return { negativeValues, positiveValues };
   };
 
+  const getTransactionOfLastDaysByBudget = (budgetId: number) => {
+    const negativeValues = lastDays
+      .map((date) =>
+        transactionList
+          .filter((txn) => txn.categoryId === budgetId)
+          .filter((txn) => txn.value < 0)
+          .filter(
+            (txn) => new Date(txn.date).toISOString().split("T")[0] === date
+          )
+          .reduce((acc, txn) => acc + txn.value, 0)
+      )
+      .map((value) => Math.abs(value));
+
+    const positiveValues = lastDays
+      .map((date) =>
+        transactionList
+          .filter((txn) => txn.categoryId === budgetId)
+          .filter((txn) => txn.value > 0)
+          .filter(
+            (txn) => new Date(txn.date).toISOString().split("T")[0] === date
+          )
+          .reduce((acc, txn) => acc + txn.value, 0)
+      )
+      .map((value) => value);
+
+    return { negativeValues, positiveValues };
+  };
+
   const { negativeValues, positiveValues } = getTransactionOfLastDays();
 
   const transfromDaysToWeekDays = (days: number) => {
@@ -101,31 +121,96 @@ export default function TransactionChartPage() {
     return { weekDay: transfromDaysToWeekDays(day), date };
   });
 
-  const data = {
-    labels: lastDaysFormatter.map((item) => `${item.weekDay} / ${item.date}`),
-    datasets: [
-      {
-        label: "Gasto Total no Dia",
-        data: negativeValues,
-        backgroundColor: "#df4444",
-        categoryPercentage: 0.5,
-        hoverBackgroundColor: "#df8888",
-        borderWidth: 1,
+  const options: ApexOptions = {
+    chart: {
+      toolbar: {
+        show: false,
       },
-      {
-        label: "Receita Total no Dia",
-        data: positiveValues,
-        backgroundColor: "#4444df",
-        categoryPercentage: 0.5,
-        hoverBackgroundColor: "#8888df",
-        borderWidth: 1,
+      zoom: {
+        enabled: false,
       },
-    ],
+    },
+    legend: {
+      onItemClick: {
+        toggleDataSeries: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+      },
+    },
+    dataLabels: {
+      enabled: passedDays <= 10 || !chartType.includes("budget"),
+    },
+    xaxis: {
+      categories: lastDaysFormatter.map((item) => item.date),
+    },
+    tooltip: {
+      enabled: true,
+      y: {
+        formatter: (val: number) => `R$ ${val.toFixed(2)}`.replace(".", ","),
+      },
+    },
   };
 
-  const incomeData = { ...data, datasets: [data.datasets[1]] };
+  const seriesBudgetsContructor = (type: "income" | "expense") => {
+    if (type === "income") {
+      const series = budgetList.map((bdgt) => ({
+        name: bdgt.name,
+        color: bdgt.color,
+        data: [...getTransactionOfLastDaysByBudget(bdgt.id).positiveValues],
+      }));
+      return series;
+    } else if (type === "expense") {
+      const series = budgetList.map((bdgt) => ({
+        name: bdgt.name,
+        color: bdgt.color,
+        data: [...getTransactionOfLastDaysByBudget(bdgt.id).negativeValues],
+      }));
+      return series;
+    }
+  };
 
-  const expenseData = { ...data, datasets: [data.datasets[0]] };
+  const baseSeries = [
+    {
+      name: "Receitas",
+      data: positiveValues,
+      color: "#4444df",
+    },
+    {
+      name: "Despesas",
+      data: negativeValues,
+      color: "#df4444",
+    },
+  ];
+  const incomeSeries = [baseSeries[0]];
+  const expenseSeries = [baseSeries[1]];
+
+  const incomeBudgetSeries = seriesBudgetsContructor("income");
+  const expenseBudgetSeries = seriesBudgetsContructor("expense");
+
+  let typeOfChart = null;
+  switch (chartType) {
+    case "all":
+      typeOfChart = baseSeries;
+      break;
+    case "income":
+      typeOfChart = incomeSeries;
+      break;
+    case "expense":
+      typeOfChart = expenseSeries;
+      break;
+    case "budget-income":
+      typeOfChart = incomeBudgetSeries;
+      break;
+    case "budget-expense":
+      typeOfChart = expenseBudgetSeries;
+      break;
+    default:
+      typeOfChart = baseSeries;
+      break;
+  }
 
   return (
     <div className="flex flex-col h-[calc(100vh-104px)]">
@@ -143,27 +228,59 @@ export default function TransactionChartPage() {
           maxLimit={24}
           step="1"
         />
-        <Select
-          list={[
-            { id: "all", name: "Todos", value: "all" },
-            { id: "income", name: "Receitas", value: "income" },
-            { id: "expenses", name: "Despesas", value: "expenses" },
-          ]}
-          state={chartType}
-          setState={setChartType as Dispatch<SetStateAction<string | number>>}
-          className="z-30"
-        />
       </div>
-      <div className="relative mx-auto w-9/10 min-h-1/2">
-        <Bar
-          options={{ maintainAspectRatio: false, responsive: true }}
-          data={
-            chartType === "all"
-              ? data
-              : chartType === "income"
-              ? incomeData
-              : expenseData
-          }
+      <section className="self-end flex flex-col w-[95%] mt-2 gap-2 bg-chetwode-blue-200 p-2 rounded-lg">
+        <h2 className="subtitle">Controlador</h2>
+        <div className="flex gap-4">
+          <div className="flex flex-col p-2 rounded-lg bg-chetwode-blue-300">
+            <h3 className="subsubtitle">Geral:</h3>
+            <Radio
+              state={chartType === "all"}
+              setState={() => setChartType("all")}
+              name="chartType"
+              id="chartType"
+              label="Todos"
+            />
+            <Radio
+              state={chartType === "income"}
+              setState={() => setChartType("income")}
+              name="chartType"
+              id="chartType"
+              label="Receita"
+            />
+            <Radio
+              state={chartType === "expense"}
+              setState={() => setChartType("expense")}
+              name="chartType"
+              id="chartType"
+              label="Gasto"
+            />
+          </div>
+          <div className="flex flex-col p-2 rounded-lg bg-chetwode-blue-300">
+            <h3 className="subsubtitle">Orçamento:</h3>
+            <Radio
+              state={chartType === "budget-income"}
+              setState={() => setChartType("budget-income")}
+              name="chartType"
+              id="chartType"
+              label="Receita + Orçamento"
+            />
+            <Radio
+              state={chartType === "budget-expense"}
+              setState={() => setChartType("budget-expense")}
+              name="chartType"
+              id="chartType"
+              label="Gasto + Orçamento"
+            />
+          </div>
+        </div>
+      </section>
+      <div className="relative mx-auto w-9/10 h-full">
+        <Chart
+          height={"100%"}
+          options={options}
+          series={typeOfChart}
+          type="bar"
         />
       </div>
     </div>
