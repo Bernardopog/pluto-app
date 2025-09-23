5a89e405ae89a18071185ffc5f2e5c6fe3d60750
Bernardo Poggioni <hckranonymous2@hotmail.com>
2025-09-23 11:34:01 -0300

    feat(frontend): create theme toggler

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Navbar from "./layout/Navbar";
 import Modal from "./layout/Modal";
 import ActionMessage from "./components/ActionMessage";
+import ThemeToggler from "./layout/ThemeToggler/ThemeToggler";
+import { cookies } from "next/headers";
 
 const dmSans = DM_Sans({
   weight: "variable",
@@ -15,14 +17,17 @@ export const metadata: Metadata = {
   description: "A personal finance app",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const theme = (await cookies()).get("theme")?.value;
+
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={`${theme ?? ""}`}>
       <body className={`${dmSans.className} antialiased`}>
+        <ThemeToggler themeIsDark={theme === "dark"} />
         <ActionMessage />
         <Modal />
         <Navbar />
