98ebd951400dd1e9fb6b865d863554adcb9ed618
Bernardo Poggioni <hckranonymous2@hotmail.com>
2025-11-19 13:05:06 -0300

    feat(frontend/budget): just show chart in wider screens

--- src/components/BudgetPage/BudgetChart/BudgetChartShowController.tsx
+++ src/components/BudgetPage/BudgetChart/BudgetChartShowController.tsx
@@ -15,7 +15,7 @@ export default function BudgetChartShowController({
   addToExcluded,
 }: IBudgetChartShowControllerProps) {
   return (
-    <div>
+    <div className='mt-8'>
       <h3 className='subsubtitle'>Remover do Gráfico:</h3>
       <ul className='grid grid-cols-1 gap-2 md:grid-cols-3'>
         {budgetList.map((bdgt) => (
--- src/layout/Budget/BudgetChart.tsx
+++ src/layout/Budget/BudgetChart.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { ApexOptions } from 'apexcharts';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   BudgetChartAssigned,
   BudgetChartController,
@@ -62,54 +62,80 @@ export default function BudgetChart() {
 
   const budgetListLength = budgetList.length;
 
+  const [showChart, setShowChart] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth >= 720) {
+      setShowChart(true);
+    }
+
+    window.addEventListener('resize', () => {
+      const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      setShowChart(width >= 720);
+    });
+
+    return () => {
+      window.removeEventListener('resize', () => {
+        const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        setShowChart(width >= 720);
+      });
+    }
+  }, []);
+
   return (
     <section
       id='budget-chart'
       className='base-card flex flex-col min-w-0 overflow-x-auto scrollbar-style scrollbar-thinner'
     >
       <h2 className='subtitle'>Gráfico</h2>
-      {budgetListLength > 0 && (
-        <BudgetChartController
-          chartToShow={chartToShow}
-          setChartToShow={setChartToShow}
-        />
-      )}
-      {budgetListLength > 0 ? (
-        <div className='flex justify-center items-center flex-1 relative min-h-9/10'>
-          {chartToShow !== 'spend' && (
-            <BudgetChartAssigned
-              budgetList={budgetListFiltered}
-              options={options}
-              isOverlay={chartToShow === 'overlay'}
-            />
-          )}
-          {chartToShow === 'overlay' && (
-            <BudgetChartSpend
-              budgetList={budgetListFiltered}
-              options={options}
-              isOverlay={true}
-            />
-          )}
-          {chartToShow === 'spend' && (
-            <BudgetChartSpend
-              budgetList={budgetListFiltered}
-              options={options}
-              isOverlay={false}
-            />
-          )}
-        </div>
-      ) : (
+      {showChart ? <>
+        {budgetListLength > 0 && (
+          <BudgetChartController
+            chartToShow={chartToShow}
+            setChartToShow={setChartToShow}
+          />
+        )}
+        {budgetListLength > 0 ? (
+          <div className='flex justify-center items-center flex-1 relative min-h-9/10 mt-16'>
+            {chartToShow !== 'spend' && (
+              <BudgetChartAssigned
+                budgetList={budgetListFiltered}
+                options={options}
+                isOverlay={chartToShow === 'overlay'}
+              />
+            )}
+            {chartToShow === 'overlay' && (
+              <BudgetChartSpend
+                budgetList={budgetListFiltered}
+                options={options}
+                isOverlay={true}
+              />
+            )}
+            {chartToShow === 'spend' && (
+              <BudgetChartSpend
+                budgetList={budgetListFiltered}
+                options={options}
+                isOverlay={false}
+              />
+            )}
+          </div>
+        ) : (
+          <p className='flex items-center justify-center flex-1 font-bold text-lg italic text-chetwode-blue-950/75 dark:text-chetwode-blue-50/75'>
+            Crie um orçamento para ver o gráfico...
+          </p>
+        )}
+        {budgetListLength > 0 && (
+          <BudgetChartShowController
+            addToExcluded={addToExcluded}
+            budgetList={budgetList}
+            excludedId={excludedId}
+            removeFromExcluded={removeFromExcluded}
+          />
+        )}
+      </> : (
         <p className='flex items-center justify-center flex-1 font-bold text-lg italic text-chetwode-blue-950/75 dark:text-chetwode-blue-50/75'>
-          Crie um orçamento para ver o gráfico...
+          Acesse em um dispositivo com tela maior...
         </p>
-      )}
-      {budgetListLength > 0 && (
-        <BudgetChartShowController
-          addToExcluded={addToExcluded}
-          budgetList={budgetList}
-          excludedId={excludedId}
-          removeFromExcluded={removeFromExcluded}
-        />
       )}
     </section>
   );
